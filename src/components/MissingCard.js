30b4880b2a116538979dc60457ae743dcfe4863c
Martin Ek <mail@ekmartin.com>
2016-09-18 09:58:02 -0400

    Fix profile image

--- src/components/MissingCard.js
+++ src/components/MissingCard.js
@@ -3,19 +3,11 @@ import React from 'react';
 const MissingCard = ({ profile }) => (
   <div className="card">
     <div className="card-image">
-      <figure className="image is-4by3">
-        <img src="http://placehold.it/600x550" alt="" />
-      </figure>
+      <img src={profile.imageUrl} alt={profile.name} />
     </div>
     <div className="card-content">
-      <div className="columns">
-        <div className="column">
-          <h3 className="title is-3">{profile.name}</h3>
-        </div>
-        <div className="column">
-          <h3 className="subtitle is-3 has-text-right">{profile.age}</h3>
-        </div>
-      </div>
+      <h3 className="title is-3">{profile.name}</h3>
+      <h5 className="subtitle is-5">Age: {profile.ageNow}</h5>
     </div>
   </div>
 );
