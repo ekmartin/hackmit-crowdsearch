8e5a0ae3ed088479232d4350d90fa16668c4782d
Mustafa Abdelhamid <abdelha5@uwm.edu>
2016-09-17 22:55:19 -0400

    Added a redirect function
    Redirect function for Facebook was added

--- src/components/Contribute.js
+++ src/components/Contribute.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 
 export default class Contribute extends Component {
   componentDidMount() {
-    // stuff
+    const { base } = this.props;
+    base.authGetOAuthRedirectResult((err, authData) => {
+      if (err) return console.log('Failed redirect', err);
+      if (authData.user) {
+        console.log('Logged in!', authData.user);
+      } else {
+        base.authWithOAuthRedirect('facebook');
+      }
+    });
   }
 
   render() {
