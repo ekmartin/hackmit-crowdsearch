b124e19b6aae46b8b464e6a53e6c2ad6e0604027
Mustafa Abdelhamid <abdelha5@uwm.edu>
2016-09-18 10:29:17 -0400

    Last Seen Location
    Added a feature to be able to show last seen location of missing
    person. Retrieves location from Firebase DB

--- src/components/Information.js
+++ src/components/Information.js
@@ -41,6 +41,17 @@ export default class Information extends Component {
                 {profile.message}
               </p>
               <DetailedInformation profile={profile} />
+              <h1 className="title is-3 has-text-centered">
+                Last Seen Location
+              </h1>
+              <iframe
+                width="635"
+                height="450"
+                frameBorder="0"
+                src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyCASLqNTKj9ON4kksYcvGFRCPjUgEH0gEw
+                &q=${profile.locationLastSeen}`}
+                allowFullScreen
+              />
             </div>
           </div>
           <ShareBox profile={profile} />
